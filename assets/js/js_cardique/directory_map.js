29c9e7c7fba8095e74869e955d7b0eddcf5387ce
NefretySanchez <nefrety.sanchez@vasslatam.com>
2023-02-03 11:15:50 -0500

    update directory page

--- assets/js/js_cardique/directory_map.js
+++ assets/js/js_cardique/directory_map.js
@@ -1,13 +1,13 @@
 function onclick_map(id) {
   document.getElementById("view-directory-" + id).style.display = "block";
   localStorage.setItem("id_directory", id);
+  $(".map-section svg")[0].style.display = "none";
 }
-
-document.addEventListener("scroll", remove_modal);
 
 function remove_modal() {
   let id_info = localStorage.getItem("id_directory");
   if (id_info !== null) {
     document.getElementById("view-directory-" + id_info).style.display = "none";
+    $(".map-section svg")[0].style.display = "block";
   }
 }
